08f859a5e6ef94140e982b47dffefc43cd59ab76
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2019-05-01 20:35:56 -0500

    Update name checker to handle an array w a first and last name

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-let name = prompt('What is your name?');
+let nameArr = [];
 
+nameArr[0] = prompt('What is your first name?');
+nameArr[1] = prompt('What is your last name?');
 
 // Function declaration
-function errorCheck(someName){
-    if(!someName){
-        someName = prompt('Como te llamas?');
+function errorCheck(name){
+    if(!name[0] || !name[1]){
+        name[0] = prompt("Nah, for real. What's your first name?");
+        name[1] = prompt('What da heck is your last name?');
 
         // Recursion- function calls itself again
-        someName = errorCheck(someName);
+        name = errorCheck(name);
     }
 
-    return someName;
+    return name;
 }
 
 // Function expression
 // let errorCheck = function() {}
 
-name = errorCheck(name);
+nameArr = errorCheck(nameArr);
 
 
-document.querySelector('#greeting').innerHTML = `<p> Hello ${name}! </p>`;
+document.querySelector('#greeting').innerHTML = `<p> Hello, ${nameArr[0]} ${nameArr[1]} ! </p>`;
 
