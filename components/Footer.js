aff999323e8f80f612b0478e17b6a45b192dbc92
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2019-05-06 19:06:11 -0500

    Adding components directory

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+export default `  <footer>
+&copy; 2019 Abby Kaemmerer
+<a href="https://github.com/abbykaemmerer" target="_blank">
+  <span class="fab fa-github"></span"></a>
+<a href="https://www.linkedin.com/in/abby-kaemmerer-80331766/" target="_blank">
+  <span class="fab fa-linkedin"></span></a>
+<a href="https://www.facebook.com/abby.kaemmerer">
+  <span class="fab fa-facebook"></span></a>
+</footer>`;
---
+++ components/Header.js
@@ -0,0 +1,6 @@
+export default `<header>
+<div id="greeting"></div>
+
+<h1>Abby Kaemmerer's Portfolio</h1>
+
+</header>`;
