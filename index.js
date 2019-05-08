759e6adf111d0b9e950c128482d09bd8e04018c0
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2019-05-07 21:17:51 -0500

    Adding event listeners to the navigation

--- index.js
+++ index.js
@@ -17,3 +17,37 @@ ${Header(state)}
 ${Main(state)}
 ${Footer(state)}
 `;
+
+// querySelectorAll returns a NodeList which is an Array Like Object
+const links = document.querySelectorAll('nav a');
+
+// We need to iterate or loop over the elements inside the NodeList
+// while keyword executes a block of code while some condition is true
+// A while loop does require a 'global' iterator variable
+let i = 0;
+
+while(i < links.length){
+    links[i].addEventListener('click', (event) => {
+        event.preventDefault();
+        console.log(event.target.textContent);
+    });
+
+    i++;
+}
+
+links.forEach((link) => {
+    links[i].addEventListener('click', (event) => {
+        event.preventDefault();
+        console.log('this is from click handler', event.target.textContent);
+    });
+});
+
+// Imperative that we make sure to increment our iterator variable - else we will end up in an infinite loop
+// We need to add the addEventListener to each
+
+// Create click event handlers that will respond when we click one of our navigation elements
+document.querySelector('nav a').addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log('I was clicked.');
+    console.log(event.target.textContent);
+});
