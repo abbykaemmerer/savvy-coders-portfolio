78a345012b3fa19cd02bf535c4cffc6920223e7f
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2019-05-29 18:59:08 -0500

    Add Blog store data

---
+++ store/Blog.js
@@ -0,0 +1,9 @@
+import links from './Links';
+
+export default {
+    // Value of pageContent corresponds with one of the modules contained in Pages
+    'pageContent': 'Blog',
+    'title': 'My Blog',
+    'links': links,
+    'posts': []
+};
