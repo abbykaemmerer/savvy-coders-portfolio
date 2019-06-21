7911a9a9ac93070bca240982240c83a4e0854b0c
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2019-06-21 11:01:48 -0500

    Adding capstone page and sylizing

---
+++ components/pages/Capstone.js
@@ -0,0 +1,13 @@
+export default (state) => `<section>
+<div class="sec1">
+<h3 class="capstone-title"> ${state.subtitle}</h3>
+<p> 4PAWS is an eCommerce web application designed to help pet owners find the best products for their pets. It is a state driven, modular single page application with functional components created using JavaScript ES6, HTML 5, CSS 3, JSON, Node.js, npm, and git. </p>
+<ul>Future plans for this project include:
+<li> Using an API to import real life products for each condition </li>
+<li> Adding more animal species </li>
+</ul>
+<div class="paws">
+Visit https://abby-kaemmerer-capstone.netlify.com to experience 4PAWS!
+</div>
+
+</section>`;
---
+++ store/Capstone.js
@@ -0,0 +1,8 @@
+import links from './Links';
+
+export default {
+    'pageContent': 'Capstone',
+    'title': 'Capstone Project',
+    'subtitle': '4PAWS',
+    'links': links
+};
