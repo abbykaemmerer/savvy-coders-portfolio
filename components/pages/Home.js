e972d9488715b3d3e8bf769d95d3b8159c98a0ad
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2019-06-25 09:47:21 -0500

    Adding a width to the figure caption

--- components/pages/Home.js
+++ components/pages/Home.js
@@ -5,7 +5,6 @@ export default (state) => `<section>
 <img
   src="${state.image}"
   width= 500px
-  height= 700px
   alt="Toothless, the most beautiful cat in the world.">
 <figcaption>Toothless of the House Kaemmerer, First of his Name, The Unburnt, King of the First Kittens and Protector of the Swamp.  Breaker of Toys and Child of Abby.</figcaption>
 </figure>
