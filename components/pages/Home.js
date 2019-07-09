dde90bc3f60c929eddf4fa27a3e5d01a9766b274
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2019-07-09 12:34:24 -0500

    Use cloudinary for main pic

--- components/pages/Home.js
+++ components/pages/Home.js
@@ -3,7 +3,7 @@ export default (state) => `<section>
 <h2 class="sec1title"></h2>
 <figure>
 <img
-  src="${state.image}"
+  src="https://res.cloudinary.com/abbykaemmerer/image/upload/v1562693595/me_kixbn2.jpg"
   width= 500px
   alt="Me, attempting to model in front of a nice blue wall.">
 </figure>
--- store/Home.js
+++ store/Home.js
@@ -1,11 +1,8 @@
 import links from './Links';
 
-import img from '../img/me.JPG';
-
 export default {
     // Value of pageContent corresponds with one of the modules contained in Pages
     'pageContent': 'Home',
     'title': 'Abby Kaemmerer',
     'links': links,
-    'image': img
 };
