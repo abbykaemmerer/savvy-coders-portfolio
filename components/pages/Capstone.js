490a35eb0576823cf2ddf68fe56b4a85090559ac
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2019-07-24 10:30:48 -0500

    Change link and nav colors

--- components/pages/Capstone.js
+++ components/pages/Capstone.js
@@ -1,14 +1,15 @@
-export default (state) => `<section>
+export default (state) => `
 <div class="sec1">
-<h3 class="capstone-title"> ${state.subtitle}</h3>
-<p> 4PAWS is an eCommerce web application designed to help pet owners find the best products for their pets. It is a state driven, modular single page application with functional components created using JavaScript ES6, HTML 5, CSS 3, JSON, Node.js, npm, and git. </p>
+  <h3 class="capstone-title"> ${state.subtitle}</h3>
+  <p> 4PAWS is an eCommerce web application designed to help pet owners find the best products for their pets. It is a state driven, modular single page application with functional components created using JavaScript ES6, HTML 5, CSS 3, JSON, Node.js, npm, and git.
+  </p>
 <ul>Future plans for this project include:
 <li> Using an API to import real life products for each condition </li>
 <li> Adding more animal species </li>
 </ul>
+</div>
 
 <div class="paws">
 <a href="https://abby-kaemmerer-capstone.netlify.com">Visit 4PAWS!</a>
 </div>
-
-</section>`;
+`;
--- components/pages/Wordpress.js
+++ components/pages/Wordpress.js
@@ -1,9 +1,9 @@
-export default (state) => `<section>
+export default (state) => `
 <div class="sec1">
 <h3 class="capstone-title"> ${state.subtitle}</h3>
 <p>I created a web page for my side business, Fazed Faces, using the platform Wordpress. This website showcases my abilities as a make-up artist and displays images of different make-up looks on clients as well as a description of the look we created.
+</div>
 <div class="paws">
 <a href="https://fazedfaces.wordpress.com">Visit Fazed Faces!</a>
 </div>
-
-</section>`;
+`;
