5c1a18b2da01556efd2a01c60852fca2b7a37ea0
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2020-01-07 17:28:58 -0600

    Add links to project titles

---
+++ components/pages/Projects.js
@@ -0,0 +1,19 @@
+export default () => `
+<div class="sec1">
+<p>
+Since graduating SavvyCoders and working at TDK Technologies, I have wanted to continue learning and growing as a Front-End Developer. I have created these projects, landing pages, and games in order to do so.
+</p>
+
+<li><a href="https://thegreatestcolorgameever.netlify.com">The RGB Game</a></li>
+<li><a href="https://mueseumofcandy.netlify.com">Museum of Candy</a></li>
+<li><a href="https://catadoption.netlify.com">Cat Adoption Landing Page</a></li>
+<br>
+<ul>Works In Progress:<br></ul>
+<li><a href="https://lauren-bach.netlify.com">Bachelorette Invite</a></li>
+<li>Transitioning my portfolio to a more responsive application</li>
+</ul>
+</div>
+<div class="paws">
+<a href="https://github.com/abbykaemmerer">Visit my GitHub page!</a>
+</div>
+`;
---
+++ store/Projects.js
@@ -0,0 +1,7 @@
+import links from './Links';
+
+export default {
+    'pageContent': 'Projects',
+    'title': 'Practice Projects',
+    'links': links
+};
