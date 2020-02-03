949b75b606817b76d62d671ef99397d3cc28f5bd
Abby Kaemmerer <kaemmerer.abby@gmail.com>
2020-02-03 14:16:21 -0600

    Update about me

--- components/pages/About.js
+++ components/pages/About.js
@@ -4,22 +4,16 @@ export default () => `
   If you'd like to learn a bit about me, continue reading. If not, here is a picture of my <a href='https://res.cloudinary.com/abbykaemmerer/image/upload/v1562688662/Tooth_wyb1cq.jpg'> cat </a>.
   </p>
   <p class="aboutpara">
-    I graduated in 2016 from the University of Missouri with a Bachelor's degree in Fisheries and Wildlife Sciences and a minor in Biological Sciences. I had always planned on working with animals and devoting my career to learning more about them and how to help them. After years of frustrating and misleading jobs, I decided to change things up.
-    <br>
-    <br>
-    <span>Enter: SavvyCoders.</span>
-    <br>
-    <br>
-    I applied to SavvyCoders with little to no knowledge of coding, and an overwhelming desire to change my career. Throughout this program, I learned to really love and become passionate about web development. I have always had a knack for technology and computers but completing this bootcamp gave me the skills I needed to begin my career as a developer.
+  I currently work as a developer in an Agile environment for a IT consulting firm where we work mostly with AngularJS, Java, SQL, JavaScript, HTML, CSS, BitBucket, Jenkins, ALM, and Swagger. After graduating from the University of Missouri, I decided to continue my education by enrolling in a Front-End Development bootcamp. We learned JavaScript, Node.js, HTML, CSS, version control, GitHub, Netlify, and Trello. From my education and professional experience, I have learned how to optimize my time while working with many different teams and people. My experiences have taught me to be persistent, curious and efficient while also maintaining my creativity and passion for web development.
   </p>
 <div>
   <ul class="aboutfacts">
   <span>
   Fun Facts:
   </span>
-  <li>My cat's name is Toothless. He has teeth.</li>
-    <li>I've watched every Marvel and Star Wars movie over four times.</li>
-    <li>I can wiggle my ears.</li>
-    <li>I used to have a snake named Ludacris and three turtles named Abbie, Abi, and Abbey.</li>
+  <li>&#8226; My cat's name is Toothless. He has teeth.</li>
+  <li>&#8226; I've watched every Marvel and Star Wars movie over four times.</li>
+  <li>&#8226; I can wiggle my ears.</li>
+  <li>&#8226; I used to have a snake named Ludacris and three turtles named Abbie, Abi, and Abbey.</li>
   </ul>
 </div>`;
